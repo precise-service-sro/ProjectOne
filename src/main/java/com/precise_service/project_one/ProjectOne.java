0c08e5060edd0fbd676c7ea1a0216d3c9b456c8c
Zdeněk Vacek <zdenek.vacek@leveris.com>
2018-10-09 06:22:52 +0200

    NO_TICKET - initial application / hello world

---
+++ src/main/java/com/precise_service/project_one/ProjectOne.java
@@ -0,0 +1,11 @@
+package com.precise_service.project_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOne {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectOne.class, args);
+    }
+}
