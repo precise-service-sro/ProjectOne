3c081b36e0b8f598efa11d44ec6079ca1cca40aa
Zdeněk Vacek <zdenek.vacek@leveris.com>
2018-12-12 16:13:31 +0100

    #34 - fotky komponenty

--- ProjectOne/webapp/src/main/java/com/precise_service/project_one/web/common/FotografieDetailBean.java
+++ ProjectOne/webapp/src/main/java/com/precise_service/project_one/web/common/FotografieDetailBean.java
@@ -1,12 +1,8 @@
-package com.precise_service.project_one.web.nemovitost;
+package com.precise_service.project_one.web.common;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.faces.context.FacesContext;
-import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Named;
 
 import org.bson.types.ObjectId;
@@ -19,19 +15,12 @@ import org.springframework.data.mongodb.core.query.Query;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.gridfs.model.GridFSFile;
-import com.precise_service.project_one.entity.adresa.Stat;
-import com.precise_service.project_one.entity.filter.OsobaFilter;
 import com.precise_service.project_one.entity.nemovitost.Nemovitost;
-import com.precise_service.project_one.entity.nemovitost.NemovitostDispozice;
-import com.precise_service.project_one.entity.nemovitost.NemovitostDruhVlastnictvi;
-import com.precise_service.project_one.entity.nemovitost.NemovitostTyp;
-import com.precise_service.project_one.entity.osoba.Osoba;
 import com.precise_service.project_one.web.AbstractBean;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-import static com.precise_service.project_one.entity.SouborTyp.AVATAR_FOTO;
 import static com.precise_service.project_one.entity.SouborTyp.FOTOGRAFIE_NEMOVITOSTI;
 import static com.precise_service.project_one.web.ApplicationConstants.ID_UZIVATEL;
 import static com.precise_service.project_one.web.ApplicationConstants.METADATA_ATTRIBUTE_CONTENT_TYPE;
@@ -42,24 +31,22 @@ import static com.precise_service.project_one.web.ApplicationConstants.METADATA_
 @Slf4j
 @Data
 @Named
-public class NemovitostDetailBean extends AbstractBean {
+public class FotografieDetailBean extends AbstractBean {
 
   private Nemovitost nemovitost;
-  private List<Osoba> osobaList;
 
-  private List<Nemovitost> nemovitostList;
-  private List<NemovitostTyp> nemovitostTypList;
-  private List<NemovitostDruhVlastnictvi> nemovitostDruhVlastnictviList;
-  private List<NemovitostDispozice> nemovitostDispoziceList;
-
-  // seznam zobrazovanych obrazku
+  // poradove cislo / index zobrazovaneho obrazku
   private int fotografieIndex = 0;
+
   private StreamedContent zobrazitObrazek;
   private StreamedContent zobrazit;
 
+  public void init(Nemovitost nemovitost) {
+    this.nemovitost = nemovitost;
+  }
+
   public void posunoutDopredu() {
     fotografieIndex = (fotografieIndex + 1) % nemovitost.getFotografie().size();
-    System.out.println(fotografieIndex);
   }
 
   public void posunoutDozadu() {
@@ -67,7 +54,6 @@ public class NemovitostDetailBean extends AbstractBean {
       fotografieIndex = nemovitost.getFotografie().size();
     }
     fotografieIndex = (fotografieIndex - 1) % nemovitost.getFotografie().size();
-    System.out.println(fotografieIndex);
   }
 
   public void smazatFotografii() {
@@ -82,8 +68,9 @@ public class NemovitostDetailBean extends AbstractBean {
     fotografieIndex = 0;
   }
 
-  public void nastavitFotografii(FileUploadEvent event) {
-    System.out.println("fadsfasfas");
+  public StreamedContent stahnoutFotografii() {
+    // TODO: implement
+    return new DefaultStreamedContent();
   }
 
   public void nastavitAvatarFoto2(FileUploadEvent event) {
@@ -119,40 +106,10 @@ public class NemovitostDetailBean extends AbstractBean {
     return zobrazitObrazek(objectId);
   }
 
-  public StreamedContent zobrazitObrazek(String idParam) {
-
-    String idFotografie = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("idFotografie");
-
-    if (idFotografie == null) {
-      return null;
-    }
-
-    GridFSFile gridFSFile = gridFsTemplate.findOne(
-        new Query(Criteria
-            .where("_id").is(idFotografie)
-            .and(METADATA_QUERY_PREFIX + METADATA_ATTRIBUTE_SOUBOR_TYP).is(FOTOGRAFIE_NEMOVITOSTI)
-        )
-    );
-
-    if (gridFSFile == null) {
-      return null;
-    }
-    String filename = gridFSFile.getFilename();
-    String contentType = (String) gridFSFile.getMetadata().get(METADATA_QUERY_PREFIX + METADATA_ATTRIBUTE_CONTENT_TYPE);
-    try {
-      zobrazitObrazek = new DefaultStreamedContent(gridFsTemplate.getResource(filename).getInputStream(), contentType);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return zobrazitObrazek;
-  }
-
   public StreamedContent zobrazitObrazek(ObjectId objectId) {
     if (objectId == null) {
-      return null;
+      return new DefaultStreamedContent();
     }
-
-    String id = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("idFotografie");
 
     GridFSFile gridFSFile = gridFsTemplate.findOne(
         new Query(Criteria
@@ -162,7 +119,7 @@ public class NemovitostDetailBean extends AbstractBean {
     );
 
     if (gridFSFile == null) {
-      return null;
+      return new DefaultStreamedContent();
     }
     String filename = gridFSFile.getFilename();
     String contentType = (String) gridFSFile.getMetadata().get(METADATA_QUERY_PREFIX + METADATA_ATTRIBUTE_CONTENT_TYPE);
@@ -172,41 +129,5 @@ public class NemovitostDetailBean extends AbstractBean {
       e.printStackTrace();
     }
     return zobrazitObrazek;
-  }
-
-  public void init() {
-    // pokud nemam vybranou zadnou nemovitost, tak vytahuji prvni nemovitost z DB
-    if (nemovitost == null) {
-      log.error("Není vybrána žádná nemovitosti pro zobrazení detailu");
-      return;
-    }
-
-    Osoba prihlasenyUzivatel = loginBean.getPrihlasenyUzivatel();
-    osobaList = osobaService.getOsobaList(new OsobaFilter()
-        .setIdPrihlasenyUzivatel(prihlasenyUzivatel.getId())
-    );
-    nemovitostList = nemovitostService.getNemovitostListByVlastnik(prihlasenyUzivatel.getId());
-
-    nemovitostTypList = Arrays.asList(NemovitostTyp.values());
-    nemovitostDruhVlastnictviList = Arrays.asList(NemovitostDruhVlastnictvi.values());
-    nemovitostDispoziceList = Arrays.asList(NemovitostDispozice.values());
-    nemovitostDispoziceList = Arrays.asList(NemovitostDispozice.values());
-  }
-
-  public void ulozitZmenuNemovitosti() throws IOException {
-    log.trace("ulozitZmenuNemovitosti()");
-    nemovitost = nemovitostService.putNemovitost(nemovitost);
-    showInfoMessage("Uloženo", "Úprava nemovitosti " + nemovitost.getNazev() + " byla uložena");
-    routerBean.goToNemovitostPrehledBean();
-  }
-
-  public void zrusitZmenuNemovitosti() throws IOException {
-    log.trace("zrusitZmenuNemovitosti()");
-    showInfoMessage("Zrušeno", "Přidání/úprava nemovitosti byla zrušena");
-    routerBean.goToNemovitostPrehledBean();
-  }
-
-  public void zmenaVybraneNemovitosti(final AjaxBehaviorEvent event) {
-    showInfoMessage("Změněno", "Byla vybráná nemovitost " + nemovitost.getIdentifikaceNemovitosti());
   }
 }
